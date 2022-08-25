285a41a014c9546c9b7f78050ea45ef49769e6f5
Alejandro Tomasella <aletomasella09@gmail.com>
2022-08-25 18:19:18 -0300

    Ejercicio 1 Clase 4

--- Clase Nro 3/Ejercicio Nro 2.py
+++ Clase Nro 3/Ejercicio Nro 2.py
@@ -3,3 +3,4 @@ x = input("Peso: ")
 y = input("Estatura: ")
 
 print("Tu IMC es: " + str(round(float(x)/(float(y)*float(y)),2)))
+
---
+++ Clase Nro 4/Ejercicio Nro 1.py
@@ -0,0 +1,6 @@
+print("Ingrese su edad")
+edad = input("Edad: ")
+if int(edad) >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
