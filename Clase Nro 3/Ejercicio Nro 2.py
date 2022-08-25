29d26d6e90defc06d90315fa08e8c40db7bf000f
Alejandro Tomasella <aletomasella09@gmail.com>
2022-08-25 16:21:54 -0300

    Adding rounding

--- Clase Nro 3/Ejercicio Nro 2.py
+++ Clase Nro 3/Ejercicio Nro 2.py
@@ -2,4 +2,4 @@ print("Ingresa tu peso en KG y estatura en metros")
 x = input("Peso: ")
 y = input("Estatura: ")
 
-print("Tu IMC es: " + str(float(x)/(float(y)*float(y))))
+print("Tu IMC es: " + str(round(float(x)/(float(y)*float(y)),2)))
