c8dd87850209ae488a805468b2e997f2a9e01847
Alejandro Tomasella <aletomasella09@gmail.com>
2022-08-25 16:11:45 -0300

    Ejercicio 1 Clase 3

---
+++ Clase Nro 3/Ejercicio Nro 1.py
@@ -0,0 +1,38 @@
+name = "Alejanro"
+lastName = "Tomasella"
+age = 28
+email = "aletomasella09@gmail.com"
+tel = 123123645
+isMarried = False
+hasChildren = False
+friendsList = ["Alejandro", "Tomas", "Juan", "Pedro", "Luis", "Ramiro"]
+moviesSeen = {
+    "movie1": "The Godfather",
+    "movie2": "The Shawshank Redemption",
+    "movie3": "The Godfather II",
+    "movie4": "The Godfather III"
+}
+
+def Printer():
+    print(name)
+    print(type(name))
+    print(lastName)
+    print(type(lastName))
+    print(age)
+    print(type(age))
+    print(email)
+    print(type(email))
+    print(tel)
+    print(type(tel))
+    print(isMarried)
+    print(type(isMarried))
+    print(hasChildren)
+    print(type(hasChildren))
+    print(friendsList)
+    print(type(friendsList))
+    print(moviesSeen)
+    print(type(moviesSeen))
+    return
+
+
+Printer()
